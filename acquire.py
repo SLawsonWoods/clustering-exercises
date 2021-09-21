ce1e27094939b72eb924aac02bfbc300a05ee7fd
Sarah Lawson Woods <Sarah.lawsonwoods@gmail.com>
2021-09-20 21:35:20 -0500

    Finishing up wrangling exercise using zillow data.

--- acquire.py
+++ acquire.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 from env import host, username, password
 
-###################### Acquire Titanic Data ######################
+###################### Acquire Zillow Data ######################
 
 def get_connection(db, username=username, host=host, password=password):
     '''
@@ -18,11 +18,36 @@ def get_connection(db, username=username, host=host, password=password):
     
 def new_zillow_data():
     '''
-    This function reads the titanic data from the Codeup db into a df,
+    This function reads the zillow data from the Codeup db into a df,
     write it to a csv file, and returns the df.
     '''
     # Create SQL query.
-    sql_query = 'SELECT * FROM passengers'
+    sql_query = ('''SELECT *
+    FROM properties_2017
+    LEFT OUTER JOIN airconditioningtype 
+    USING(airconditioningtypeid) 
+    LEFT OUTER JOIN architecturalstyletype
+    USING(architecturalstyletypeid) 
+    LEFT OUTER JOIN buildingclasstype 
+    USING(buildingclasstypeid) 
+    LEFT OUTER JOIN heatingorsystemtype
+    USING(heatingorsystemtypeid) 
+    LEFT OUTER JOIN predictions_2017
+    ON predictions_2017.id = properties_2017.id
+    INNER JOIN (
+    SELECT id, MAX(transactiondate) as last_trans_date 
+    FROM predictions_2017
+    GROUP BY id
+    ) predictions ON predictions.id = properties_2017.id AND predictions_2017.transactiondate = predictions.last_trans_date
+    LEFT OUTER JOIN propertylandusetype
+    USING(propertylandusetypeid)
+      LEFT OUTER JOIN storytype
+    USING(storytypeid)
+    LEFT OUTER JOIN typeconstructiontype
+    USING(typeconstructiontypeid)
+    JOIN unique_properties
+      ON unique_properties.parcelid = properties_2017.parcelid
+    WHERE latitude IS NOT NULL and longitude IS NOT NULL;''')
     
     # Read in DataFrame from Codeup db.
     df = pd.read_sql(sql_query, get_connection('zillow'))
@@ -51,26 +76,45 @@ def get_zillow_data():
         
     return df
 
-###################### Acquire Iris Data ######################
+###################### Acquire Zillow Data ######################
 
 def new_zillow_data():
     '''
     This function reads the iris data from the Codeup db into a df.
     '''
     sql_query = """
-                SELECT 
-                    species_id,
-                    species_name,
-                    sepal_length,
-                    sepal_width,
-                    petal_length,
-                    petal_width
-                FROM measurements
-                JOIN species USING(species_id)
+                SELECT *
+    FROM properties_2017
+    LEFT OUTER JOIN airconditioningtype 
+    USING(airconditioningtypeid) 
+    LEFT OUTER JOIN architecturalstyletype
+    USING(architecturalstyletypeid) 
+    LEFT OUTER JOIN buildingclasstype 
+    USING(buildingclasstypeid) 
+    LEFT OUTER JOIN heatingorsystemtype
+    USING(heatingorsystemtypeid) 
+    LEFT OUTER JOIN predictions_2017
+    ON predictions_2017.id = properties_2017.id
+    INNER JOIN (
+    SELECT id, MAX(transactiondate) as last_trans_date 
+    FROM predictions_2017
+    GROUP BY id
+    ) predictions ON predictions.id = properties_2017.id AND predictions_2017.transactiondate = predictions.last_trans_date
+    LEFT OUTER JOIN propertylandusetype
+    USING(propertylandusetypeid)
+      LEFT OUTER JOIN storytype
+    USING(storytypeid)
+    LEFT OUTER JOIN typeconstructiontype
+    USING(typeconstructiontypeid)
+    JOIN unique_properties
+      ON unique_properties.parcelid = properties_2017.parcelid
+    WHERE latitude IS NOT NULL and longitude IS NOT NULL; 
+
+
                 """
     
     # Read in DataFrame from Codeup db.
-    df = pd.read_sql(sql_query, get_connection('iris_db'))
+    df = pd.read_sql(sql_query, get_connection('zillow'))
     
     return df
 
@@ -80,7 +124,7 @@ def get_zillow_data():
     This function reads in iris data from Codeup database, writes data to
     a csv file if a local file does not exist, and returns a df.
     '''
-    if os.path.isfile('iris_df.csv'):
+    if os.path.isfile('zillow.csv'):
         
         # If csv file exists read in data from csv file.
         df = pd.read_csv('iris_df.csv', index_col=0)
--- summarize.py
+++ summarize.py
@@ -1,126 +1,48 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import env
+
 
 from sklearn.model_selection import train_test_split
-from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
 
-###################### Prepare Iris Data ######################
 
-def prep_iris(df):
 
-    '''Prepares acquired Iris data for exploration'''
-    
-    # drop column using .drop(columns=column_name)
-    df = df.drop(columns='species_id')
-    
-    # remame column using .rename(columns={current_column_name : replacement_column_name})
-    df = df.rename(columns={'species_name':'species'})
-    
-    # create dummies dataframe using .get_dummies(column_name,not dropping any of the dummy columns)
-    dummy_df = pd.get_dummies(df['species'], drop_first=False)
-    
-    # join original df with dummies df using .concat([original_df,dummy_df], join along the index)
-    df = pd.concat([df, dummy_df], axis=1)
-    
-    return df
-
-###################### Prepare Iris Data With Split ######################
-
-def split_data(df):
+def summarize(df):
     '''
-    take in a DataFrame and return train, validate, and test DataFrames; stratify on species.
-    return train, validate, test DataFrames.
+    summarize will take in a single argument (a pandas dataframe)
+    and output to console various statistics on said dataframe, including:
+    # .head()
+    # .info()
+    # .describe()
+    # value_counts()
+    # observation of nulls in the dataframe
     '''
-    
-    # splits df into train_validate and test using train_test_split() stratifying on species to get an even mix of each species
-    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.species)
-    
-    # splits train_validate into train and validate using train_test_split() stratifying on species to get an even mix of each species
-    train, validate = train_test_split(train_validate, 
-                                       test_size=.3, 
-                                       random_state=123, 
-                                       stratify=train_validate.species)
-    return train, validate, test
-
-
-def prep_iris_with_split(df):
-    '''Prepares acquired Iris data for exploration'''
-    
-    # drop column using .drop(columns=column_name)
-    df = df.drop(columns='species_id')
-    
-    # remame column using .rename(columns={current_column_name : replacement_column_name})
-    df = df.rename(columns={'species_name':'species'})
-    
-    # create dummies dataframe using .get_dummies(column_name,not dropping any of the dummy columns)
-    dummy_df = pd.get_dummies(df['species'], drop_first=False)
-    
-    # join original df with dummies df using .concat([original_df,dummy_df], join along the index)
-    df = pd.concat([df, dummy_df], axis=1)
-    
-    # split data into train/validate/test using split_data function
-    train, validate, test = split_data(df)
-    
-    return train, validate, test
-
-def prep_titanic(df):
-    '''
-    This function take in the titanic data acquired by get_titanic_data,
-    Returns prepped train, validate, and test dfs with embarked dummy vars,
-    deck dropped, and the mean of age imputed for Null values.
-    '''
-    
-    # drop rows where embarked/embark town are null values
-    df = df[~df.embarked.isnull()]
-    
-    # encode embarked using dummy columns
-    titanic_dummies = pd.get_dummies(df.embarked, drop_first=True)
-    
-    # join dummy columns back to df
-    df = pd.concat([df, titanic_dummies], axis=1)
-    
-    # drop the deck column
-    df = df.drop(columns='deck')
-    
-    # split data into train, validate, test dfs
-    train, validate, test = titanic_split(df)
-    
-    # impute mean of age into null values in age column
-    train, validate, test = impute_mean_age(train, validate, test)
-    
-    return train, validate, test
-
-def titanic_split(df):
-    '''
-    This function take in the titanic data acquired by get_titanic_data,
-    performs a split and stratifies survived column.
-    Returns train, validate, and test dfs.
-    '''
-    train_validate, test = train_test_split(df, test_size=.2, 
-                                        random_state=123, 
-                                        stratify=df.survived)
-    train, validate = train_test_split(train_validate, test_size=.3, 
-                                   random_state=123, 
-                                   stratify=train_validate.survived)
-    return train, validate, test
-
-
-def impute_mean_age(train, validate, test):
-    '''
-    This function imputes the mean of the age column for
-    observations with missing values.
-    Returns transformed train, validate, and test df.
-    '''
-    # create the imputer object with mean strategy
-    imputer = SimpleImputer(strategy = 'mean')
-    
-    # fit on and transform age column in train
-    train['age'] = imputer.fit_transform(train[['age']])
-    
-    # transform age column in validate
-    validate['age'] = imputer.transform(validate[['age']])
-    
-    # transform age column in test
-    test['age'] = imputer.transform(test[['age']])
-    
-    return train, validate, test
+    print('=====================================================\n\n')
+    print('Dataframe head: ')
+    print(df.head(3).to_markdown())
+    print('=====================================================\n\n')
+    print('Dataframe info: ')
+    print(df.info())
+    print('=====================================================\n\n')
+    print('Dataframe Description: ')
+    print(df.describe().to_markdown())
+    num_cols = [col for col in df.columns if df[col].dtype != 'O']
+    cat_cols = [col for col in df.columns if col not in num_cols]
+    print('=====================================================')
+    print('DataFrame value counts: ')
+    for col in df.columns:
+        if col in cat_cols:
+            print(df[col].value_counts())
+        else:
+            print(df[col].value_counts(bins=10, sort=False))
+    print('=====================================================')
+    print('nulls in dataframe by column: ')
+    print(nulls_by_col(df))
+    print('=====================================================')
+    print('nulls in dataframe by row: ')
+    print(nulls_by_row(df))
+    print('============================================')
